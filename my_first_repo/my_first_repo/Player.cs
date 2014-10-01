a6cbe4c0630daca4c5b220f4c598afbada3952f4
unknown <t018327c@k106-16.student.staffs.ac.uk>
2014-10-01 11:29:54 +0100

    Added player class

---
+++ my_first_repo/my_first_repo/Player.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace my_first_repo
+{
+	public class Player
+	{
+		public Player ()
+		{
+			string playerName;
+			
+			//Player x and y coordinates
+			int x;
+			int y;
+			
+			//Player status
+			int health;
+			int mana;
+			int stamina;
+		}
+		
+		public int Update (int x, int y)
+		{
+			return x;
+			return y;
+		}
+	}
+}
+
